560078b0128f0ba73803e2ea13b9311b82ae7353
heyradcode <betterthanbestest@gmail.com>
2026-07-14 12:39:43 -0400

    feat: remove webhook tools from MCP

--- shared/src/tools/index.ts
+++ shared/src/tools/index.ts
@@ -8,9 +8,6 @@ import { registerGetWatchlistChanges } from "./get-watchlist-changes.js";
 import { registerAddToWatchlist } from "./add-to-watchlist.js";
 import { registerRemoveFromWatchlist } from "./remove-from-watchlist.js";
 import { registerGetAccount } from "./get-account.js";
-import { registerCreateWebhook } from "./create-webhook.js";
-import { registerListWebhooks } from "./list-webhooks.js";
-import { registerDeleteWebhook } from "./delete-webhook.js";
 export function registerAllTools(server: McpServer, apiKey: string) {
   registerGetSummary(server, apiKey);
   registerGetOhlcv(server, apiKey);
@@ -21,7 +18,4 @@ export function registerAllTools(server: McpServer, apiKey: string) {
   registerAddToWatchlist(server, apiKey);
   registerRemoveFromWatchlist(server, apiKey);
   registerGetAccount(server, apiKey);
-  registerCreateWebhook(server, apiKey);
-  registerListWebhooks(server, apiKey);
-  registerDeleteWebhook(server, apiKey);
 }
